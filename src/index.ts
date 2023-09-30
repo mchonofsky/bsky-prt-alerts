264d491b08c5099b3e564287377deb39d8f3f2c5
mchonofsky <mchonofsky@gmail.com>
2023-09-30 08:18:07 -0500

    turn dry run off

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
 const argv = process.argv.map(x => parseInt(x)).filter(x =>!Number.isNaN(x))
-let dryRun = true;
+let dryRun = false;
 
 if (argv.length == 0) {
   console.log('setting dry run true')
+  // this is if the cache doesn't exist
   dryRun = true;
 }
 
