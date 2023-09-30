f4476cd4b9843554cc9cdd7cefb1f439aa671182
mchonofsky <mchonofsky@gmail.com>
2023-09-29 22:41:56 -0500

    set dryrun true until we're ready

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
 const argv = process.argv.map(x => parseInt(x)).filter(x =>!Number.isNaN(x))
-let dryRun = false;
+let dryRun = true;
 
 if (argv.length == 0) {
   console.log('setting dry run true')
