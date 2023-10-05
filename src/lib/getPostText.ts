58e75c92aec88a039cca2b06a9f48fb7c2e4ca8f
mchonofsky <mchonofsky@gmail.com>
2023-10-05 17:59:02 -0500

    remove 'headline' field, just use description field

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -2,7 +2,8 @@ import type {CTAAlert} from './cta_types.js'
 
 export default async function getPostText(alert_: CTAAlert) {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  var text = `${alert_.Headline}: ${alert_.ShortDescription}`;
+  var text = `${alert_.ShortDescription}`;
+  text = text.trim();
   if (text.length > 300) {
     text = text.slice(0, 250) + '... ' + alert_.AlertURL['#cdata-section']
   }
