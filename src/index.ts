9e34c368d0609de88494c6907f911cb3c6ad7656
mchonofsky <mchonofsky@gmail.com>
2024-07-02 12:05:05 -0400

    revert filter change and improve log

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,8 @@ const texts = await Bot.run(getPostText, { parameter: argv[0], dryRun: dryRun })
 
 if (dryRun) {
   console.log("this is a dry run. not posting. would have posted:");
-  texts.map((text: string) => console.log(`[${new Date().toISOString()}] Posted: "${text}"`));
+} else {
+  console.log("actually posted:");
 }
+
+texts.map((text: string) => console.log(`[${new Date().toISOString()}] Posted: "${text}"`));
