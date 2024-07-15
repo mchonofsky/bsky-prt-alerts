70b2ef8644e9a073ca6cdabd098ada12da7e0f6d
mchonofsky <mchonofsky@gmail.com>
2024-07-15 12:41:20 -0400

    add emoji cupport to getPostText

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -3,9 +3,30 @@ import type {CTAAlert} from './cta_types.js'
 export default async function getPostText(alert_: CTAAlert) {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
   var text = `${alert_.ShortDescription}`;
+  const emojis = 
+    {
+      red: "🟥",
+      brown: "🟫",
+      blue: "🟦",
+      green: "🟩",
+      purple: "🟪",
+      orange: "🟧",
+      pink: "🩷",
+      yellow: "🟨",
+      bus: "🚍"
+    }
+  
   text = text.trim();
+  Object.keys(emojis).forEach(key => {
+    re = RegExp(`(${key} line)`, "i");
+    text = text.replace(re, `${emojis[[key]]} $1 $1`)
+  })
+  if ( text.includes("buses") ) {
+    text = `${emojis.bus} ${text}`;
+  }
   if (text.length > 300) {
     text = text.slice(0, 250) + '... ' + alert_.AlertURL['#cdata-section']
   }
+  
   return text;
 }
